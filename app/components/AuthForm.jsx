c725c8188873e8db1f5981f9a1faa91197a36d22
DevUttesh <developeruttesh@gmail.com>
2023-12-28 16:53:55 +0500

    changes !!

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -11,7 +11,7 @@ export default function AuthForm(){
         view="magic_link"
         showLinks={false}
         providers={[]}
-        redirectTo='https://watch-list-plum.vercel.app/auth/callback'
+        redirectTo='https://utteshwatch.vercel.app/auth/callback'
         appearance={{
             theme: 'dark',
             button: {
